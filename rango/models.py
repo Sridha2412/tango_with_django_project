ac21c617b2a3103f72eb8ba9aa589e64a0710bc7
Sridha Agarwal <2208414a@student.gla.ac.uk>
2017-02-02 15:55:06 +0000

    passed all of chapter 5 tests

--- rango/models.py
+++ rango/models.py
@@ -3,19 +3,21 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+        name = models.CharField(max_length=128, unique=True)
+        views = models.IntegerField(default = 0)
+        likes = models.IntegerField(default = 0)
 
-    class Meta:
-        verbose_name_plural = 'Categories'
-        
-    def __str__(self):
-        return self.name
+        class Meta:
+                verbose_name_plural = 'Categories'
+
+        def __str__(self):
+                return self.name
 
 class Page(models.Model):
-    category = models.ForeignKey(Category)
-    title = models.CharField(max_length=128)
-    url = models.URLField()
-    views = models.IntegerField(default = 0)
+        category = models.ForeignKey(Category)
+        title = models.CharField(max_length=128)
+        url = models.URLField()
+        views = models.IntegerField(default = 0)
 
-    def __str__(self):
-        return self.title
+        def __str__(self):
+                return self.title
