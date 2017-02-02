880c496e08a33c9e5a8c10dc059754595fe2faf7
Sridha Agarwal <2208414a@student.gla.ac.uk>
2017-02-02 13:55:37 +0000

    added views and likes

--- populate_rango.py
+++ populate_rango.py
@@ -5,7 +5,7 @@ import django
 django.setup()
 from rango.models import Category, Page
 
-def populate()
+def populate():
 	python_pages = [
 	{"title": "Official Python Tutorial",
 	"url": "http://docs.python.org/2/tutorial/"},
@@ -17,7 +17,7 @@ def populate()
 	
 	django_pages = [
 	{"title": "Official Django Tutorial",
-	"url": "https://docs.djangoproject.com/en/1.9/intro/tutor"}
+	"url": "https://docs.djangoproject.com/en/1.9/intro/tutor"},
 	{"title": "Django Rocks",
 	"url": "http://www.djangorocks.com/"},
 	{"title":"How to Tango with Django", 
@@ -31,15 +31,15 @@ def populate()
 	"url":"http://flask.pocoo.org"}
 	]
 	
-	cats = {"Python": {"pages": python_pages}, 
-			"Django": {"pages": django_pages},
-			"Other Frameworks": {"pages": other_pages}}
+	cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64}, 
+			"Django": {"pages": django_pages, "views": 64, "likes":32},
+			"Other Frameworks": {"pages": other_pages, "views":32, "likes":16}}
 			
 	
 	for cat, cat_data in cats.items():
-		c = add_cat(Cat)
+		c = add_cat(cat)
 		for p in cat_data["pages"]:
-			add_page(C, p["title"], p["url"])
+			add_page(c, p["title"], p["url"])
 			
 	for c in Category.objects.all():
 		for p in Page.objects.filter(category=c):
@@ -52,8 +52,10 @@ def add_page(cat, title, url, views=0):
 	p.save()
 	return p
 
-def add_cat(name):
+def add_cat(name, views = 0, likes = 0):
 	c = Category.objects.get_or_create(name=name)[0]
+	c.likes = likes
+	c.views = views
 	c.save()
 	return c
 	
