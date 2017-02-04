07f6218bd55d2b054304c5fd772904ff7c66dd82
Sridha Agarwal <sridha2412@gmail.com>
2017-02-04 22:21:39 +0000

    Completed chapter 8

--- rango/views.py
+++ rango/views.py
@@ -18,6 +18,8 @@ def index(request):
 
 
 def about(request):
+        print(request.method)
+        print(request.user)
         return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
