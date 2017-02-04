d15850d236560c44e73f31f0d593db9d5ac22b18
Sridha Agarwal <sridha2412@gmail.com>
2017-02-04 15:03:37 +0000

    finished up to chapter 7 exercises

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,23 @@
+from rango.forms import CategoryForm
+
 from django.shortcuts import render
 
 from django.http import HttpResponse
 
 from rango.models import Category
 from rango.models import Page
+
+def index(request):
+        category_list = Category.objects.order_by('-likes')[:5]
+        page_list = Page.objects.order_by('-views')[:5]
+        
+        context_dict = {'categories': category_list, 'pages': page_list}
+        
+        return render(request, 'rango/index.html', context_dict)
+
+
+def about(request):
+        return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
         context_dict = {}
@@ -23,14 +37,21 @@ def show_category(request, category_name_slug):
 
         return render(request, 'rango/category.html', context_dict)
 
-def index(request):
-        category_list = Category.objects.order_by('-likes')[:5]
-        page_list = Page.objects.order_by('-views')[:5]
-        
-        context_dict = {'categories': category_list, 'pages': page_list}
-        
-        return render(request, 'rango/index.html', context_dict)
 
+def add_category(request):
+        form = CategoryForm()
 
-def about(request):
-        return render(request, 'rango/about.html', {})
+        if request.method == 'POST':
+                form = CategoryForm(request.POST)
+
+                if form.is_valid():
+                        form.save(commit=True)
+
+                        return index(request)
+
+                else:
+                        print(form.errors)
+
+        return render(request, 'rango/add_category.html', {'form': form})
+
+                
