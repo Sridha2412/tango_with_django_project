c0810feac875bce9d6570bc11295b8c6fa561620
Sridha Agarwal <sridha2412@gmail.com>
2017-02-04 23:10:07 +0000

    created user registration view and template

--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 from django.template.defaultfilters import slugify
-
+from django.contrib.auth.models import User
 from django.db import models
 
 class Category(models.Model):
@@ -27,3 +27,11 @@ class Page(models.Model):
 
         def __str__(self):
                 return self.title
+
+class UserProfile(models.Model):
+        user = models.OneToOneField(User)
+        website = models.URLField(blank=True)
+        picture = models.ImageField(upload_to='profile_images', blank=True)
+
+        def __str__(self):
+                return self.user.username()
