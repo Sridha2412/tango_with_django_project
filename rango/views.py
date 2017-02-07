b6396e97af4dc43293de0a23feb911f1e1bc942b
Sridha Agarwal <2208414a@student.gla.ac.uk>
2017-02-07 11:27:58 +0000

    Almost done with chapter10

--- rango/views.py
+++ rango/views.py
@@ -8,32 +8,38 @@ from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
 
+def get_server_side_cookie(request, cookie, default_val=None):
+        val = request.session.get(cookie)
+        if not val:
+                val = default_val
+        return val
 
-def visitor_cookie_handler(request, response):
-        visits = int(request.COOKIES.get('visits', '1'))
-
-        last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+def visitor_cookie_handler(request):
+        visits = int(get_server_side_cookie(request, 'visits', '1'))
+        last_visit_cookie = get_server_side_cookie(request, 'last_visit',
+                                                   str(datetime.now()))
         last_visit_time = datetime.strptime(last_visit_cookie[:-7],
                                             '%Y-%m-%d %H:%M:%S')
 
         if(datetime.now() - last_visit_time).seconds > 0:
                 visits = visits + 1
-                response.set_cookie('last_visit', str(datetime.now()))
+                request.session['last_visit'] = str(datetime.now())
         else:
                 visits = 1
-                response.set_cookie('last_visit', last_visit_cookie)
+                request.session['last_visit'] = last_visit_cookie
 
-        response.set_cookie('visits', visits)
+        request.session['visits'] = visits
 
 def index(request):
+        request.session.set_test_cookie()
         category_list = Category.objects.order_by('-likes')[:5]
         page_list = Page.objects.order_by('-views')[:5]
         context_dict = {'categories': category_list, 'pages': page_list}
         
-        response = render(request, 'rango/index.html', context_dict)
-
-        visitor_cookie_handler(request, response)
-
+        visitor_cookie_handler(request)
+        context_dict['visits'] = request.session['visits']
+        
+        response = render(request, 'rango/index.html', context = context_dict)
         return response
 
 
