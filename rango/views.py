03b6dfcc1921e3a1acf8653df6134441da6fdae3
Sridha Agarwal <2208414a@student.gla.ac.uk>
2017-02-08 16:50:51 +0000

    finsihed chapter 10

--- rango/views.py
+++ rango/views.py
@@ -21,7 +21,7 @@ def visitor_cookie_handler(request):
         last_visit_time = datetime.strptime(last_visit_cookie[:-7],
                                             '%Y-%m-%d %H:%M:%S')
 
-        if(datetime.now() - last_visit_time).seconds > 0:
+        if(datetime.now() - last_visit_time).days > 0:
                 visits = visits + 1
                 request.session['last_visit'] = str(datetime.now())
         else:
@@ -47,9 +47,12 @@ def about(request):
         if request.session.test_cookie_worked():
                 print("TEST COOKIE WORKED!")
                 request.session.delete_test_cookie()
-        print(request.method)
-        print(request.user)
-        return render(request, 'rango/about.html', {})
+				
+	context_dict = {}
+        visitor_cookie_handler(request)
+        context_dict['visits'] = request.session['visits']
+        
+	return render(request, 'rango/about.html', context_dict)
 
 def show_category(request, category_name_slug):
         context_dict = {}
